75a9ed842bcba5a6c50a4bc3b09e64cc1e7abe41
Leah Emerson <32882883+leahemerson@users.noreply.github.com>
2019-05-05 15:10:44 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -7,10 +7,23 @@ var url = require('url');
 // downloaded NodeJS modules
 var mime = require('mime-types');
 var multiparty = require('multiparty');
+var express = require('express');
+var sqlite3 = require('sqlite3');
+var app = express();
+var db_filename = path.join(__dirname, 'db', 'pongDB.sqlite3');
 
 var port = 8007;
 var public_dir = path.join(__dirname, 'public');
 //var public_dir = path.join(__dirname, 'tmarrinan.github.io');
+var db = new sqlite3.Database(db_filename, sqlite3.OPEN_READONLY, (err) => {
+	if (err) {
+		console.log('Error opening' + db_filename);
+	}
+	else{
+		console.log('Now connected to' + db_filename);
+	}
+});
+app.use(express.static(public_dir));
 
 var server = http.createServer((req, res) => {
     var req_url = url.parse(req.url);
@@ -39,7 +52,50 @@ var server = http.createServer((req, res) => {
         if (filename === 'upload') {
             var form = new multiparty.Form();
             form.parse(req, (err, fields) => {
-                console.log(fields);
+                console.log(fields.password[0]);
+		var username = decodeURI(fields.username[0]).replace(/\*/g, '%');
+		db.all('SELECT * FROM logins WHERE Username = ?', [username], (err,rows) => {
+		if(err)
+		{
+			//write some error code
+			console.log("invlaid user");
+			alert("User does not exsist");
+		}
+		else
+		{	
+			console.log(rows);
+			db.all('SELECT Password FROM logins WHERE Username = ?', [username], (err,actualPass) => {
+			if(err)
+			{
+				//write some error code
+				console.log("invlaid pass");
+				alert("Incorrect password");
+			}
+			else
+			{
+
+				//console.log(actualPass[0].password);
+				if(actualPass[0] && actualPass[0].password == fields.password[0])
+				{
+					res.writeHead(200, {'Content-Type': 'application/json'});
+					res.write(JSON.stringify(rows));
+					res.end();
+				}
+				else
+				{
+					console.log("invalid username or pass");
+					//window.alert("invalid username or password");
+				}
+			}
+		});
+		}
+
+
+	});
+
+
+
+
                // console.log(files);
    /*             fs.copyFile(path.join(__dirname), (err) => {
                     if (err) {
